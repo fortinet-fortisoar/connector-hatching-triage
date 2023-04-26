f126482f473e26e2fe26776a8c8556bac0a84425
okumbhar <okumbhar@IND-OKumbhar-MAC.local>
2023-04-26 15:10:59 +0530

    triage connector v1.0.0

---
+++ hatching-triage/constants.py
@@ -0,0 +1,11 @@
+""" Copyright start
+  Copyright (C) 2008 - 2023 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end """
+
+
+GET = "GET"
+POST = "POST"
+PUT = "PUT"
+DELETE = "DELETE"
